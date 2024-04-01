474379d9c0857cd79814d92715399bde843bd5d5
Ricardo <ricardomineli@outlook.com>
2024-04-01 15:01:11 -0300

    Removed useless file

--- src/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import PosterMaker from "@/app/ui/poster-maker";
-
-export default function Test() {
-  return (
-    <main className="flex min-h-screen items-center justify-between p-4">
-      <PosterMaker />
-    </main>
-  );
-}
